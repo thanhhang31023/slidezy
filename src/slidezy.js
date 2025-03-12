ad02a3c70117aca9b9d03cc5ae00927268ecda03
thanhhang31023 <thanhhang31023@gmail.com>
2025-03-12 21:24:18 +0900

    Create  moveSlide method

--- src/slidezy.js
+++ src/slidezy.js
@@ -7,6 +7,7 @@ function Slidezy(selector, options = {}) {
 
     this.opt = Object.assign({}, options);
     this.slides = Array.from(this.container.children);
+    this.currentIndex = 0;
 
     this._init();
 }
@@ -47,6 +48,10 @@ Slidezy.prototype._createNavigation = function () {
 };
 
 Slidezy.prototype.moveSlide = function (step) {
-    // ...
-    console.log(step);
+    this.currentIndex = Math.min(
+        Math.max(this.currentIndex + step, 0),
+        this.slides.length - 3
+    );
+    this.offset = -(this.currentIndex * (100 / 3));
+    this.track.style.transform = `translateX(${this.offset}%)`;
 };
