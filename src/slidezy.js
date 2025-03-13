6edc4f7764a4316405d6e09ea622fb782c3d999f
thanhhang31023 <thanhhang31023@gmail.com>
2025-03-13 22:44:34 +0900

    Add option items and  loop

--- src/slidezy.js
+++ src/slidezy.js
@@ -5,7 +5,13 @@ function Slidezy(selector, options = {}) {
         return;
     }
 
-    this.opt = Object.assign({}, options);
+    this.opt = Object.assign(
+        {
+            items: 1,
+            loop: false,
+        },
+        options
+    );
     this.slides = Array.from(this.container.children);
     this.currentIndex = 0;
 
@@ -25,6 +31,7 @@ Slidezy.prototype._createTrack = function () {
 
     this.slides.forEach((slide) => {
         slide.classList.add("slidezy-slide");
+        slide.style.flexBasis = `calc(100% / ${this.opt.items})`;
         this.track.appendChild(slide);
     });
 
@@ -48,10 +55,16 @@ Slidezy.prototype._createNavigation = function () {
 };
 
 Slidezy.prototype.moveSlide = function (step) {
-    this.currentIndex = Math.min(
-        Math.max(this.currentIndex + step, 0),
-        this.slides.length - 3
-    );
-    this.offset = -(this.currentIndex * (100 / 3));
+    if (this.opt.loop) {
+        this.currentIndex =
+            (this.currentIndex + step + this.slides.length) %
+            this.slides.length;
+    } else {
+        this.currentIndex = Math.min(
+            Math.max(this.currentIndex + step, 0),
+            this.slides.length - this.opt.items
+        );
+    }
+    this.offset = -(this.currentIndex * (100 / this.opt.items));
     this.track.style.transform = `translateX(${this.offset}%)`;
 };
